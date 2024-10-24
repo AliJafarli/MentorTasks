fef7595c792c3c924317a5603205b817d4ab375b
AliJafarli <somagni4icent@gmail.com>
2024-10-24 19:00:17 +0400

    u

---
+++ Perimetr/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] a = {{1,2,-3},{4,-5,6},{-7,-8,9}};
+
+
+        Solution solution = new Solution();
+        System.out.println(solution.count(a));
+
+    }
+}
