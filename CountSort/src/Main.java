7980504a31950894536598f0237a4a0e397bc398
AliJafarli <somagni4icent@gmail.com>
2024-11-14 18:29:05 +0400

    ++

---
+++ CountSort/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 7, 2, 1, 1, 9};
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int[] arr2 = new int[max + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr2[arr[i]]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            while (arr2[i] > 0) {
+                arr[index++] = i;
+                arr2[i]--;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
