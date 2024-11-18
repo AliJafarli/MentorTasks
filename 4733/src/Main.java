28828672474f03052dc5d665d822349084fcd7c7
AliJafarli <somagni4icent@gmail.com>
2024-11-18 18:47:05 +0400

    +

--- 4733/src/Main.java
+++ 4733/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-         Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int number = in.nextInt();
     }
-    
+
 }
