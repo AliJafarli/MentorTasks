c21bfc1e79312a8fb1c76e9b20c0e2cc649f3715
AliJafarli <somagni4icent@gmail.com>
2024-11-26 16:52:27 +0400

    task

--- Test/src/Human.java
+++ Test/src/Human.java
@@ -3,6 +3,22 @@ public class Human implements Comparable{
     String name;
     int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public Human(String name, int age) {
         this.name = name;
         this.age = age;
--- Test/src/Main.java
+++ Test/src/Main.java
@@ -7,10 +7,17 @@ public class Main {
                 new Human("Jdjkcn", 32),
                 new Human("Uncwnjec", 86))
         );
-//        Collections.sort(list);
-        list.sort(Comparator.comparing((Human h) -> h.name).thenComparing((Human h) -> h.age));
-        Iterator<Human> iterator= list.iterator();
-        while (iterator.hasNext()){
+//        Collections.sort(list); это способ сортировки по 1 параметру
+//        list.sort(Comparator.comparing((Human h) -> h.name)   //а тут по неск параметрам
+//                .thenComparing((Human h) -> h.age));
+//        Iterator<Human> iterator = list.iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+        list.sort(Comparator.comparing(Human::getName)
+                .thenComparing(Human::getAge));
+        Iterator<Human> iterator = list.iterator();
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
 
